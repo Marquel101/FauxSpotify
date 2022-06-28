2c503a9e897973de66e0f5a9ae041a1c9c89ff75
Marquel <daylensears2@gmail.com>
2022-06-28 15:59:44 -0400

    hamburger almost complete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,24 +2,42 @@ import "./Navbar.css"
 import React from 'react'
 import { Link } from "react-router-dom";
 import logo from "./spotify.jpg"
+import { useState } from "react";
 
 export default function Navbar() {
+
+  const [toggler, setToggler] = useState(false)
+
+  const toggle = () => {
+    toggler ? setToggler(false) : setToggler(true)
+  }
+  // const hamburger = document.querySelector(".hamburger")
+  // const navLinks = document.querySelector(".navlinks")
+
+  // hamburger.addEventListener("click",() => {
+  //   hamburger.classList.toggle("active");
+    
+  // })
   return (
     <div className="banner">
     <div className="navbar">
       <div className="brand">
        <img className='logo' src={logo}/>
       </div>
-        <ul className="navlinks">
-            <li className="link">Premium</li>
-            <li className="link">Support</li>
-            <li className="link">Download</li>
-            <li className="line"></li>
-            <li className="auth">Sign up</li>
-            <li className="auth">Log in</li>
+        <ul className={toggler ? "navlinks" : "navlinksactive"}>
+            <div className="links">
+              <li className="link">Premium</li>
+              <li className="link">Support</li>
+              <li className="link">Download</li>
+            </div>
+            <li className={toggler ? "line" : "lineactive"}></li>
+            <div className="auth">
+              <li className="link">Sign up</li>
+              <li className="link">Log in</li>
+            </div>
         </ul>
       
-        <div className="hamburger">
+        <div className={toggler ? "hamburger" : "hamburgeractive"} onClick={toggle}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
