5f704c5e4d21068890dbe2d0729a729eada23915
Marquel <daylensears2@gmail.com>
2022-06-29 02:17:28 -0400

    nav 99.5% complete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -6,38 +6,47 @@ import { useState } from "react";
 
 export default function Navbar() {
 
-  const [toggler, setToggler] = useState(false)
+  const [toggler, setToggler] = useState(true)
+  const [check,setCheck] = useState(false)
 
+  const hamburger = document.querySelector(".hamburger")
+  const width = window.innerWidth
+  
+  const toggleCheck = () => {
+     if (width > 991) {
+        
+        setCheck(true)
+     } else if (width < 992) {
+       setCheck(false)
+       setToggler(false)
+     }
+  }
+  window.addEventListener('resize', toggleCheck)
+ 
   const toggle = () => {
     toggler ? setToggler(false) : setToggler(true)
   }
-   const hamburger = document.querySelector(".hamburger")
-  // const navLinks = document.querySelector(".navlinks")
 
-  // hamburger.addEventListener("click",() => {
-  //   hamburger.classList.toggle("active");
-    
-  // })
   return (
     <div className="banner">
     <div className="navbar">
       <div className="brand">
        <img className='logo' src={logo}/>
-      </div>0
-        <ul className={toggler ? "" : "navlinksactive"}>
-            <div className="links">navlinks
+      </div>
+        <ul className={toggler || check  ? "navlinks" : "navlinksactive"}>
+            <div className="links">
               <li className="link">Premium</li>
               <li className="link">Support</li>
               <li className="link">Download</li>
             </div>
-            <li className={toggler ? "line" : "lineactive"}></li>
+            <li className={toggler || check ? "line" : "lineactive"}></li>
             <div className="auth">
               <li className="link">Sign up</li>
               <li className="link">Log in</li>
             </div>
         </ul>
       
-        <div className={toggler ? "hamburger" : "hamburgeractive"} onClick={toggle}>
+        <div className={toggler || check ? "hamburger" : "hamburgeractive"} onClick={toggle}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
