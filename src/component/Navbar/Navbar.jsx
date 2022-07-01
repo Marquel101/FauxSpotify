f3fc78e253c97007a6f87dc296d5d4c05bf423a0
Marquel <daylensears2@gmail.com>
2022-07-01 19:22:28 -0400

    nav 99.9 complete splsh pge strted

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -7,46 +7,46 @@ import { useState } from "react";
 export default function Navbar() {
 
   const [toggler, setToggler] = useState(true)
+  const [hide, setHide] = useState(true)
   const [check,setCheck] = useState(false)
 
-  const hamburger = document.querySelector(".hamburger")
+  
   const width = window.innerWidth
   
-  const toggleCheck = () => {
-     if (width > 991) {
-        
-        setCheck(true)
-     } else if (width < 992) {
-       setCheck(false)
-       setToggler(false)
-     }
-  }
-  window.addEventListener('resize', toggleCheck)
+  // const toggleCheck = () => {
+  //    if (width > 991) {
+       
+  //      setToggler(true)
+  //    }
+  //   }
+  // toggleCheck()
  
   const toggle = () => {
     toggler ? setToggler(false) : setToggler(true)
   }
-
+  const hider = () => {
+    hide ? setHide(false) : setHide(true)
+  }
   return (
     <div className="banner">
     <div className="navbar">
       <div className="brand">
        <img className='logo' src={logo}/>
       </div>
-        <ul className={toggler || check  ? "navlinks" : "navlinksactive"}>
+        <ul className={toggler  ? "navlinks" : "navlinksactive"}>
             <div className="links">
               <li className="link">Premium</li>
               <li className="link">Support</li>
               <li className="link">Download</li>
             </div>
-            <li className={toggler || check ? "line" : "lineactive"}></li>
+            <li className={toggler ? "line" : "lineactive"}></li>
             <div className="auth">
               <li className="link">Sign up</li>
               <li className="link">Log in</li>
             </div>
         </ul>
       
-        <div className={toggler || check ? "hamburger" : "hamburgeractive"} onClick={toggle}>
+        <div className={toggler  ? "hamburger" : "hamburgeractive"} onClick={toggle}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
--- src/screens/Splash.jsx
+++ src/screens/Splash.jsx
@@ -1,7 +1,13 @@
+import "./Splash.css"
+
 export default function Splash(params) {
     return (
-        <div>
+        <div className="splash">
             SPLASH
+            <div className="dot" id="dot1"></div>
+            <div className="dot"id="dot2"></div>
+            <div className="dot"id="dot3"></div>
+            <div className="dot"id="dot4"></div>
         </div>
     )
 };
