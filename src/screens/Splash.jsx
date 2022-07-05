2536a2d6f37b2b154968b10aec51790a55ed155b
Marquel <daylensears2@gmail.com>
2022-07-05 15:11:59 -0400

    splsh strted

--- src/screens/Splash.jsx
+++ src/screens/Splash.jsx
@@ -3,7 +3,15 @@ import "./Splash.css"
 export default function Splash(params) {
     return (
         <div className="splash">
-            SPLASH
+            <div className="intro">
+                <h1 className="heder">Listening is everything</h1>
+                <div className="support">
+                <p>Millions of songs and podcasts. No credit card needed.</p>
+                </div>
+                <div className="button">
+                    <a>GET SPOTIFY FREE</a>
+                </div>
+            </div>
             <div className="dot" id="dot1"></div>
             <div className="dot"id="dot2"></div>
             <div className="dot"id="dot3"></div>
